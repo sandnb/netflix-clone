9b74bc6b84a49d6a4e631e114f1efa2f1f810ab2
WhileTrueCoder <nithinsandilyab@gmail.com>
2022-12-26 18:03:26 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar.js'
+import { action,originals,horror} from './urls'
+import './App.css'
+import Banner from './Components/Banner/Banner'
+import RowPost from './Components/RowPost/RowPost'
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+      <RowPost url={originals}title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall  />
+      <RowPost url={horror} title='Horror' isSmall  />
+    </div>
+    
+  );
+}
+
+export default App;
